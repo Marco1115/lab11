cfb690252e161dd6b5b5859cd734bd4993a685e9
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-12-06 11:51:15 +0100

    Created the visual part of the GUI

--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.oop.reactivegui02;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.slf4j.LoggerFactory;
+
+import it.unibo.oop.JFrameUtil;
+
+import org.slf4j.Logger;
+
 import java.io.Serial;
 
 /**
@@ -10,4 +20,24 @@ public final class ConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrentGUI.class);
+    private final JLabel display = new JLabel("-5");
+
+    /**
+     * Builds a new GUI.
+     */
+    public ConcurrentGUI() {
+        super();
+        JFrameUtil.dimensionJFrame(this);
+        final JPanel pane = new JPanel();
+        pane.add(this.display);
+        final JButton up = new JButton("up");
+        final JButton down = new JButton("down");
+        final JButton stop = new JButton("stop");
+        pane.add(up);
+        pane.add(down);
+        pane.add(stop);
+        this.setContentPane(pane);
+        this.setVisible(true);
+    }
 }
