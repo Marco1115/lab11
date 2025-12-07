592f43ffba7316173fb0fca2518c1c0027e2202a
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-12-07 16:17:23 +0100

    Implemented the constructor of multi threaded sum matrix

--- 113-workers/src/test/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 113-workers/src/test/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -1,9 +1,21 @@
 package it.unibo.oop.workers02;
 
-public class MultiThreadedSumMatrix implements SumMatrix {
+/**
+ * Multithreaded implementation of the {@link SumMatrix} interface.
+ */
+public final class MultiThreadedSumMatrix implements SumMatrix {
+
+    private final int nthread;
+
+    /**
+     * @param nthread the number of threads performing the class.
+     */
+    public MultiThreadedSumMatrix(final int nthread) {
+        this.nthread = nthread;
+    }
 
     @Override
-    public double sum(double[][] matrix) {
+    public double sum(final double[][] matrix) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'sum'");
     }
