c44c8c1b09846ab95759502c2fd8a5b8f1092c2e
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-12-07 17:58:59 +0100

    Implemented the inner class worker

--- 113-workers/src/test/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 113-workers/src/test/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -20,4 +20,51 @@ public final class MultiThreadedSumMatrix implements SumMatrix {
         throw new UnsupportedOperationException("Unimplemented method 'sum'");
     }
 
+    private static final class Worker extends Thread {
+
+        private final double[][] matrix;
+        private final int startpos;
+        private final int nelem;
+        private long res;
+
+        /**
+         * Build a new worker.
+         *
+         * @param matrix
+         *            the matrix to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        private Worker(final double[][] matrix, final int startpos, final int nelem) {
+            super();
+            this.matrix = matrix;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public synchronized void run() {
+            final int rows = this.matrix.length;
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+            for (int i = startpos; i < rows && i < startpos + nelem; i++) {
+                for (final double element: this.matrix[i]) {
+                    res += element;
+                }
+            }
+        }
+
+        /**
+         * Returns the result of summing up the doubles within the matrix.
+         *
+         * @return the sum of every element in the matrix
+         */
+        public synchronized long getResult() {
+            return this.res;
+        }
+
+    }
+
 }
